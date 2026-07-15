d4c0233af276c554439ae20183b81d0277efe373
Yosimaril <yosuaimmanuelariyasalianoto@gmail.com>
2026-07-15 20:26:29 +0700

    Setting Page

--- komiku/lib/screens/setting_screen.dart
+++ komiku/lib/screens/setting_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:komiku/provider/theme_state_provider.dart';
+import 'package:komiku/static/app_theme_mode.dart';
+import 'package:provider/provider.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
@@ -10,6 +13,46 @@ class SettingScreen extends StatefulWidget {
 class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Preferences",
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 800),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Consumer<ThemeStateProvider>(
+                  builder: (context, value, child) => SwitchListTile(
+                    title: Text(
+                      "Dark Mode",
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                    value: value.themeMode == AppThemeMode.dark,
+                    onChanged: (value) {
+                      if (value) {
+                        context.read<ThemeStateProvider>().setThemeMode(
+                          AppThemeMode.dark,
+                        );
+                      } else {
+                        context.read<ThemeStateProvider>().setThemeMode(
+                          AppThemeMode.light,
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
